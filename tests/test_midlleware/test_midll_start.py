a9a1b21ea79d00f17bfa3740caa6648ceac7211d
furte <furtep2@gmail.com>
2026-07-23 15:09:53 +0600

    Попытка 12

--- tests/test_midlleware/test_midll_start.py
+++ tests/test_midlleware/test_midll_start.py
@@ -12,11 +12,11 @@ from aiogram.fsm.context import FSMContext
 class Test_Midlleware_Start():
     @pytest.mark.asyncio
     async def test_check_midlleware_start(self, mocker):
-        fake_user = AsyncMock(spec = types.User)
-        fake_user.configure_mock(username = "Test", id = 45)
+        fake_user = AsyncMock(spec=types.User)
+        fake_user.configure_mock(username="Test", id=45)
 
         fake_message = AsyncMock()
-        fake_message.configure_mock(from_user = fake_user, text = "/start", answer = AsyncMock(), bot = AsyncMock())
+        fake_message.configure_mock(from_user=fake_user, text="/start", answer=AsyncMock(), bot=AsyncMock())
 
         fake_state = AsyncMock(spec=FSMContext)
         fake_state.get_state.return_value = None
@@ -26,9 +26,17 @@ class Test_Midlleware_Start():
         fake_handler = AsyncMock()
         fake_data = {"state": fake_state}
 
+        mock_session = mocker.patch("admin_tools.users_requests.AsyncSessionLocal")
+        fake_db = AsyncMock()
+        mock_session.return_value.__aenter__.return_value = fake_db
+        mock_result = MagicMock()
+        fake_db.execute.return_value = mock_result
+        mock_result.scalar_one_or_none.return_value = None 
+
         await middleware(fake_handler, fake_message, fake_data)
 
         fake_handler.assert_awaited_once()
+
 
     @pytest.mark.asyncio
     async def test_check_midlleware_start_invalid_type(self, mocker):
